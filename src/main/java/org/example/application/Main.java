a0fe2a8e63383abf87e027e1b0be62b54f372d67
João Tamassia <joaotamassia@alunos.utfpr.edu.br>
2023-11-23 13:38:04 -0300

    First Class: Position

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,9 +1,12 @@
-package org.example;
+package org.example.application;
+
+import org.example.boardgame.Position;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
         }
     }
